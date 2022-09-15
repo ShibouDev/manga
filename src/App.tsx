1b7fd3a93690de79b4a13fa3e9914cc163ce48f3
Shibou <sadandcryalone@gmail.com>
2022-09-15 23:06:20 +0500

    add header in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-
+import Header from './components/Header'
 function App() {
   return (
     <div className="App">
-       <h1 className="text-3xl font-bold underline text-red-600">
-         Simple React Typescript Tailwind Sample
-        </h1>
+       <Header/>
     </div>
   );
 }
